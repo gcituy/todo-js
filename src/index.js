cb1df3fbd02227857258f2cae803f601cfaa6f97
Gabriel <gabriel@gcit.uy>
2020-04-12 02:44:39 -0300

    Primer Commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 
-import { saludar } from './js/componentes';
 import './styles.css';
 
-const nombre = 'Gabriel !!!';
+import { Todo, TodoList } from './classes'
+import { crearTodoHtml } from './js/componentes'
 
-saludar( nombre );
+export const todoList = new TodoList();
+
+// const tarea = new Todo('Aprender JavaScript');
+// const tarea2 = new Todo('Comprar un Unicornio');
+
+// todoList.nuevoTodo(tarea);
+// todoList.nuevoTodo(tarea2);
+
+console.log( todoList );
+
+todoList.todos.forEach( crearTodoHtml);
+
+// tarea.completado = true;
+
+//  crearTodoHtml( tarea );
+
+// Uso de localStorage y sessionStorage es igual
+//  localStorage.setItem('mi-key', 'ABC123');
+ 
+//  setTimeout( () => {
+//      localStorage.removeItem('my-key');
+//  })
